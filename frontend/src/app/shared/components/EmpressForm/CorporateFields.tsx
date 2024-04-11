9232c1f35859d7d32447a7347d6c0ce27cec2204
breno <brenomenesesdiaz@gmail.com.br>
2024-04-10 21:41:47 -0300

    validate coirporate fields

--- frontend/src/app/shared/components/EmpressForm/CorporateFields.tsx
+++ frontend/src/app/shared/components/EmpressForm/CorporateFields.tsx
@@ -1,136 +1,173 @@
-import { TextField, Box, Button } from '@mui/material'
-import { useForm } from "react-hook-form"
-import { yupResolver } from "@hookform/resolvers/yup"
-import * as yup from "yup"
+import { TextField, Box, Button } from "@mui/material";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
 interface CorporateFields {
-    corporateName: string
-    cnpj: string
-    phone: string
+  corporateName: string;
+  cnpj: string;
+  phone: string;
 }
 
 interface propUserFields {
-    HandleNext: () => void
-    HandleBack: () => void
-    activeStep: number
+  HandleNext: () => void;
+  HandleBack: () => void;
+  activeStep: number;
 }
 
 const corporateFieldSchema = yup.object({
-    corporateName: yup.string().required("campo obrigatório"),
-    cnpj: yup.string().required("campo obrigatório"),
-    phone: yup.string().required("campo obigatório")
+  corporateName: yup.string().required("campo obrigatório"),
+  cnpj: yup.string().required("campo obrigatório"),
+  phone: yup.string().required("campo obigatório"),
 })
 
-export function CorporateFields() {
+export function CorporateFields({ activeStep, HandleNext, HandleBack}: propUserFields) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(corporateFieldSchema) })
 
-    return (
-        <>
-            <TextField
-                variant='outlined'
-                label='Nome da empresa'
-                type='text'
-                name='corporateName'
-                sx={{ marginY: 2, width: 416 }}
-            />
-            <TextField
-                variant='outlined'
-                label='Cnpj da empresa'
-                type='text'
-                name='cnpj'
-                sx={{ marginY: 2, width: 416 }}
-            />
-            <TextField
-                variant='outlined'
-                label='Telefone pra contato'
-                type='text'
-                sx={{ marginY: 2, width: 416 }}
-            />
-        </>
-    )
-}
-
-interface UserFieldsValue {
-    name: string
-    email: string
-    password: string
-    passConf: string
-}
-
-interface propUserFields {
-    HandleNext: () => void
-    HandleBack: () => void
-    activeStep: number
+  const onSubmit = (data: any) => {
+    console.log(data)
+    HandleNext()
+  };
+  return (
+    <Box
+      component={"form"}
+      onSubmit={handleSubmit(onSubmit)}
+      sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
+      <TextField
+        error={errors.corporateName?.message ? true : false}
+        helperText={errors.corporateName?.message}
+        variant="outlined"
+        label="Nome da empresa"
+        type="text"
+        sx={{ marginY: 2, width: 416 }}
+        {...register("corporateName")}
+      />
+      <TextField
+        error={errors.cnpj?.message ? true : false}
+        helperText={errors.cnpj?.message}
+        variant="outlined"
+        label="cnpj"
+        type="text"
+        sx={{ marginY: 2, width: 416 }}
+        {...register("cnpj")}
+      />
+        <TextField
+          error={errors.phone?.message ? true : false}
+          helperText={errors.phone?.message}
+          variant="outlined"
+          label="phone"
+          type="text"
+          {...register("phone")}
+          sx={{ marginX: 1, marginY: 2, width: 200 }}
+        />
+      <Box sx={{ width: "100%", display: "flex", flexDirection: "row", pt: 2 }}>
+        <Button
+          color="inherit"
+          disabled={activeStep === 0}
+          onClick={HandleBack}
+          sx={{ mr: 1 }}
+        >
+          Back
+        </Button>
+        <Box sx={{ flex: "1 1 auto" }} />
+        <Button type="submit" color="primary" variant="outlined">
+          {activeStep === steps.length - 1 ? "Finish" : "Next"}
+        </Button>
+      </Box>
+    </Box>
+  )
 }
 
 const userFieldSchema = yup.object({
-    name: yup.string().required("campo obrigatório"),
-    email: yup.string().required("campo obrigatório").email("emil inválido"),
-    password: yup.string().required("campo obrigatório"),
-    passConf: yup.string().required("campo obrigatório").oneOf([yup.ref('password')], 'senhas diferentes')
-})
+  name: yup.string().required("campo obrigatório"),
+  email: yup.string().required("campo obrigatório").email("emil inválido"),
+  password: yup.string().required("campo obrigatório"),
+  passConf: yup
+    .string()
+    .required("campo obrigatório")
+    .oneOf([yup.ref("password")], "senhas diferentes"),
+});
 
-const steps = ['Informações do usuário', 'informações da empresa', 'endereço']
-export function UserFields({ activeStep, HandleNext, HandleBack }: propUserFields) {
-    const { register, handleSubmit, formState: { errors } } = useForm<UserFieldsValue>({ resolver: yupResolver(userFieldSchema) })
+const steps = ["Informações do usuário", "informações da empresa", "endereço"];
+export function UserFields({
+  activeStep,
+  HandleNext,
+  HandleBack,
+}: propUserFields) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(userFieldSchema) });
 
-    const onSubmit = (data: any) => {
-        console.log(data)
-        HandleNext()
-    }
+  const onSubmit = (data: any) => {
+    console.log(data);
+    HandleNext();
+  };
 
-    return (
-        <Box component={"form"} onSubmit={handleSubmit(onSubmit)} sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-            <TextField
-                error={errors.name?.message ? true : false}
-                helperText={errors.name?.message}
-                variant='outlined'
-                label='Nome'
-                type='text'
-                sx={{ marginY: 2, width: 416 }}
-                {...register("name")}
-            />
-            <TextField
-                error={errors.email?.message ? true : false}
-                helperText={errors.email?.message}
-                variant='outlined'
-                label='Email'
-                type='email'
-                sx={{ marginY: 2, width: 416 }}
-                {...register("email")}
-            />
-            <Box width="100%" sx={{ display: "flex", justifyContent: "center" }}>
-                <TextField
-                    error={errors.password?.message ? true : false}
-                    helperText={errors.password?.message}
-                    variant='outlined'
-                    label='Senha'
-                    type='password'
-                    {...register("password")}
-                    sx={{ marginX: 1, marginY: 2, width: 200 }}
-                />
-                <TextField
-                    error={errors.passConf?.message ? true : false}
-                    helperText={errors.passConf?.message}
-                    variant='outlined'
-                    label='Confirme a senha'
-                    type='password'
-                    {...register("passConf")}
-                    sx={{ marginX: 1, marginY: 2, width: 200 }}
-                />
-            </Box>
-            <Box sx={{ width: "100%", display: 'flex', flexDirection: 'row', pt: 2 }}>
-                <Button
-                    color="inherit"
-                    disabled={activeStep === 0}
-                    onClick={HandleBack}
-                    sx={{ mr: 1 }} >
-                    Back
-                </Button>
-                <Box sx={{ flex: '1 1 auto' }} />
-                <Button type="submit" color="primary" variant="outlined">
-                    {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
-                </Button>
-            </Box>
-        </Box>
-    )
+  return (
+    <Box
+      component={"form"}
+      onSubmit={handleSubmit(onSubmit)}
+      sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
+      <TextField
+        error={errors.name?.message ? true : false}
+        helperText={errors.name?.message}
+        variant="outlined"
+        label="Nome"
+        type="text"
+        sx={{ marginY: 2, width: 416 }}
+        {...register("name")}
+      />
+      <TextField
+        error={errors.email?.message ? true : false}
+        helperText={errors.email?.message}
+        variant="outlined"
+        label="Email"
+        type="email"
+        sx={{ marginY: 2, width: 416 }}
+        {...register("email")}
+      />
+      <Box width="100%" sx={{ display: "flex", justifyContent: "center" }}>
+        <TextField
+          error={errors.password?.message ? true : false}
+          helperText={errors.password?.message}
+          variant="outlined"
+          label="Senha"
+          type="password"
+          {...register("password")}
+          sx={{ marginX: 1, marginY: 2, width: 200 }}
+        />
+        <TextField
+          error={errors.passConf?.message ? true : false}
+          helperText={errors.passConf?.message}
+          variant="outlined"
+          label="Confirme a senha"
+          type="password"
+          {...register("passConf")}
+          sx={{ marginX: 1, marginY: 2, width: 200 }}
+        />
+      </Box>
+      <Box sx={{ width: "100%", display: "flex", flexDirection: "row", pt: 2 }}>
+        <Button
+          color="inherit"
+          disabled={activeStep === 0}
+          onClick={HandleBack}
+          sx={{ mr: 1 }}
+        >
+          Back
+        </Button>
+        <Box sx={{ flex: "1 1 auto" }} />
+        <Button type="submit" color="primary" variant="outlined">
+          {activeStep === steps.length - 1 ? "Finish" : "Next"}
+        </Button>
+      </Box>
+    </Box>
+  );
 }
--- frontend/src/app/shared/components/EmpressForm/FormEmpress.tsx
+++ frontend/src/app/shared/components/EmpressForm/FormEmpress.tsx
@@ -1,62 +1,86 @@
-import { useState, Fragment, SyntheticEvent } from "react"
-import { Box, Stepper, Step, StepLabel, Button, Typography } from '@mui/material'
-import { UserFields, AddressFields } from "./UserFields"
-import {CorporateFields} from "./CorporateFields"
+import { useState, Fragment, SyntheticEvent } from "react";
+import {
+  Box,
+  Stepper,
+  Step,
+  StepLabel,
+  Button,
+  Typography,
+} from "@mui/material";
+import { UserFields, AddressFields } from "./UserFields";
+import { CorporateFields } from "./CorporateFields";
 
-const steps = ['Informações do usuário', 'informações da empresa', 'endereço']
+const steps = ["Informações do usuário", "informações da empresa", "endereço"];
 
 export function FormEmpress() {
-    const [activeStep, setActiveStep] = useState(0)
+  const [activeStep, setActiveStep] = useState(0);
 
-    const HandleNext = () => {
-        setActiveStep((activeStep) => activeStep + 1)
-    }
+  const HandleNext = () => {
+    setActiveStep((activeStep) => activeStep + 1);
+  };
 
-    const handleBack = () => {
-        setActiveStep((activeStep) => activeStep - 1)
-    }
+  const handleBack = () => {
+    setActiveStep((activeStep) => activeStep - 1);
+  };
 
-    const handleReset = () => {
-        setActiveStep(0)
-    }
+  const handleReset = () => {
+    setActiveStep(0);
+  };
 
-    return (
-        <Box sx={{ width: '50%', margin: "auto", padding: 1 }}>
-            <Stepper activeStep={activeStep}>
-                {steps.map((label, index) => {
-                    const stepProps: { completed?: boolean } = {}
-                    return (
-                        <Step key={label} {...stepProps}>
-                            <StepLabel >{label}</StepLabel>
-                        </Step>
-                    )
-                })}
-            </Stepper>
-            {activeStep === steps.length ? (
-                <Fragment>
-                    <Typography component="div" sx={{ mt: 2, mb: 1 }}>
-                        <Box sx={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", height: 400 }}>
-                            All steps completed - you're finished
-                        </Box>
-                    </Typography>
-                    <Box sx={{ display: 'flex', justifyContent: "center", pt: 2 }}>
-                        <Button color="primary" sx={{ width: "30%" }} onClick={handleReset}>Reset</Button>
-                    </Box>
-                </Fragment>
-            ) : (
-                <Fragment>
-                    <Typography component="div" sx={{ mt: 2, mb: 1 }}>
-
-                            {activeStep + 1 === 1 && <UserFields
-                                activeStep={activeStep}
-                                HandleNext={HandleNext}
-                                HandleBack={handleBack} />}
-                            {activeStep + 1 === 2 && <CorporateFields />}
-                            {activeStep + 1 === 3 && <AddressFields />}
-
-                    </Typography>
-                </Fragment>
+  return (
+    <Box sx={{ width: "50%", margin: "auto", padding: 1 }}>
+      <Stepper activeStep={activeStep}>
+        {steps.map((label, index) => {
+          const stepProps: { completed?: boolean } = {};
+          return (
+            <Step key={label} {...stepProps}>
+              <StepLabel>{label}</StepLabel>
+            </Step>
+          );
+        })}
+      </Stepper>
+      {activeStep === steps.length ? (
+        <Fragment>
+          <Typography component="div" sx={{ mt: 2, mb: 1 }}>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+                height: 400,
+              }}
+            >
+              All steps completed - you're finished
+            </Box>
+          </Typography>
+          <Box sx={{ display: "flex", justifyContent: "center", pt: 2 }}>
+            <Button color="primary" sx={{ width: "30%" }} onClick={handleReset}>
+              Reset
+            </Button>
+          </Box>
+        </Fragment>
+      ) : (
+        <Fragment>
+          <Typography component="div" sx={{ mt: 2, mb: 1 }}>
+            {activeStep + 1 === 1 && (
+              <UserFields
+                activeStep={activeStep}
+                HandleNext={HandleNext}
+                HandleBack={handleBack}
+              />
             )}
-        </Box>
-    )
+            {activeStep + 1 === 2 && (
+              <CorporateFields
+                activeStep={activeStep}
+                HandleNext={HandleNext}
+                HandleBack={handleBack}
+              />
+            )}
+            {activeStep + 1 === 3 && <AddressFields />}
+          </Typography>
+        </Fragment>
+      )}
+    </Box>
+  );
 }
